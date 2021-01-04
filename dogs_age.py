8493aeac2d595b5fae2c686ea22eb2604d24a299
RadoslawP <radoslaw.pietraszek.r@gmail.com>
2021-01-04 20:23:22 +0100

    dzialajacy kod

--- dogs_age.py
+++ dogs_age.py
@@ -1,5 +1,5 @@
 imie_psa = input("Jak sie wabi Twoj pies? ")
 wiek_psa = input("Ile Twoj pies ma lat? ")
 ludzki_wiek =int(wiek_psa) * 7
-print ("Twoj pies "+imie_psa+" ma juz "+ludzki_wiek+" ludzkich lat.")
+print ("Twoj pies ", imie_psa, " ma juz ", ludzki_wiek, " ludzkich lat.")
 print (ludzki_wiek)
